82ad24ac33fcf076c2634788ccefbe2c71b0e257
Raghav-Pal <automation.devops@gmail.com>
2022-06-06 06:47:45 +0000

    update

--- Scripts/Day5/GitHubDemo1/Script1654497235622.groovy
+++ Scripts/Day5/GitHubDemo1/Script1654497235622.groovy
@@ -17,3 +17,5 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
+WebUI.acceptAlert()
+
